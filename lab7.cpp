f62b03604cae61bbad4f6dc50ba81561a3aad2b2
TonySavich <92541859+TonySavich@users.noreply.github.com>
2022-09-26 17:46:04 +0200

    Add files via upload

---
+++ lab7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+
+void shel(int mas[], int n)
+{
+    int i, j, step;
+    int tmp;
+    for (step = n / 2; step > 0; step /= 2)
+        for (i = step; i < n; i++)
+        {
+            tmp = mas[i];
+            for (j = i; j >= step; j -= step)
+            {
+                if (tmp < mas[j - step])
+                    mas[j] = mas[j - step];
+                else
+                    break;
+            }
+            mas[j] = tmp;
+        }
+}
+
+
+
+int main()
+{
+    int mas[10];
+    int n = 10;
+    for (int i = 0; i < n; i++) {
+        mas[i] = rand() % 10;
+        std::cout << mas[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    shel(mas, n);
+
+    for (int i = 0; i < n; i++) {
+        std::cout << mas[i] << " ";
+    }
+
+
+}
